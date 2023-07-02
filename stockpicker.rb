e416e11142d7d481c8a538de993e8b6805c138fc
Iain Macleod <macleod@live.co.uk>
2023-07-02 20:10:46 +0100

    updated to loop through array checking each price against all subsequent prices to find the best profit.

---
+++ stockpicker.rb
@@ -0,0 +1,20 @@
+def stockpicker (prices)
+
+    highest_profit = 0
+    sell_day = 0
+    buy_day = 0
+
+    prices.each_with_index do |price, index|
+        for i in index..(prices.size - 1)
+            if prices[i] - price > highest_profit
+                highest_profit = prices[i] - price
+                sell_day = i
+                buy_day = index
+            end
+        end
+    end
+    puts "Buy:  #{buy_day} -- Sell:#{sell_day}"
+    puts "#{prices[sell_day]} - #{prices[buy_day]} = #{prices[sell_day]-prices[buy_day]}"
+end
+
+stockpicker([17,3,6,9,15,8,6,1,10])
